4052b3ed15108c25ca6fa9c4a5626cdafb274d99
kaessp <philipp.kaess@tngtech.com>
2025-01-24 09:51:31 +0100

    Fill personal information

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "James Butler",
-    // In the following fiels you can either give a single string,
+    name: "Philipp Kaess",
+    // In the following fields you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["Ehm, what?"],
+    associations: ["Continuous Integration / Continuous Development", "Testing"],
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins, maybe?",
+    tools: "None",
     // What do you want to learn in this workshop?
-    expectations: ["what you are talking about"],
+    expectations: ["A basic idea of how CI/CD works, and when to use it."],
   },
 });
